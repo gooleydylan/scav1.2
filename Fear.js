d087ecf5dbb808e16da81245f9c168592e431fd0
gooleydylan <32112570+gooleydylan@users.noreply.github.com>
2017-09-27 17:11:20 -0400

    Create Fear.js

---
+++ Fear.js
@@ -0,0 +1,5 @@
+window.alert("Are you ready to face your worst fears???");
+var fr = window.prompt("What animal are you most afraid of?");
+function RevealFears(){
+document.getElementById("fear").innerHTML="<p><a href=https:'https://www.youtube.com/watch?v=1jidTPJ6iaU'>Man being eaten by "+fr  
+}
